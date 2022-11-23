2da31f8c44d7b7257a0c64d4773c1a02482a8d11
kuni3933 <57650186+kuni3933@users.noreply.github.com>
2022-11-23 10:27:49 +0900

    [Update]fix: onValueを分けた

--- ChildThread/onValue_angle.js
+++ ChildThread/onValue_angle.js
@@ -13,21 +13,16 @@ const raspPiSerialNumber = await getSerialNumber()
     return "83ed5c72";
   });
 
-onValue(ref(db, `RaspPi/${raspPiSerialNumber}/Is_Locked`), (snapshot) => {
-  let isLocked = snapshot.val();
-
-  console.log("childThread: onValue.js");
-  // console.log(`snapshot: ${isLocked}`);
-  parentPort.postMessage({ isLocked: isLocked });
-});
-
 onValue(ref(db, `RaspPi/${raspPiSerialNumber}/Angle`), (snapshot) => {
-  const onValue_Angle = snapshot.val();
+  //ログ
+  console.log("childThread: onValue.js");
+
+  //変更値[オブジェクト]をonValue_Angleに格納
+  const onValue_angle = snapshot.val();
   const jsonData = JSON.parse(fs.readFileSync("config.json", "utf-8"));
-  jsonData.Angle = onValue_Angle;
+  jsonData.Angle = onValue_angle;
   fs.writeFileSync("config.json", JSON.stringify(jsonData));
 
-  console.log("childThread: onValue.js");
-  //console.log("childThread: onValue.js");
-  parentPort.postMessage({ Angle: onValue_Angle });
+  //親スレッドにonValue_AngleにPOST送信
+  parentPort.postMessage({ angle: onValue_angle });
 });
--- index.js
+++ index.js
@@ -4,9 +4,9 @@ import { Door } from "./Class/Door.js";
 const door = await Door.initDoor();
 door.execute_state();
 
-const onValueThread = new Worker("./ChildThread/onValue.js");
-onValueThread.on("message", (msg) => {
-  const { isLocked, Angle } = msg;
+const onValue_isLocked_Thread = new Worker("./ChildThread/onValue_isLocked.js");
+onValue_isLocked_Thread.on("message", (msg) => {
+  const { isLocked } = msg;
 
   if (isLocked != undefined) {
     console.log(`onValueThread: Received { isLocked: ${isLocked} }`);
@@ -15,9 +15,14 @@ onValueThread.on("message", (msg) => {
       door.execute_state();
     }
   }
+});
 
-  if (Angle != undefined) {
-    console.log(`onValueThread: Received { Angle: ${JSON.stringify(Angle)} }`);
+const onValue_angle_Thread = new Worker("./ChildThread/onValue_angle.js");
+onValue_angle_Thread.on("message", (msg) => {
+  const { angle } = msg;
+
+  if (angle != undefined) {
+    console.log(`onValueThread: Received { angle: ${JSON.stringify(angle)} }`);
   }
 });
 
