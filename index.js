9d0191c3804e877c8e4ed9444c711515f1f65594
kuni3933 <57650186+kuni3933@users.noreply.github.com>
2022-12-01 12:58:30 +0900

    [Update]fix: パスの修正

--- index.js
+++ index.js
@@ -49,26 +49,28 @@ Atomics.store(sharedUint8Array, 0, false); // 初期値: isLocked = false
 Atomics.store(sharedUint8Array, 1, false); // 初期値: isOpened = false
 
 //* ロック/アンロック角度設定の読込/更新を行う子スレッドを起動
-const onValue_angle_Thread = new Worker("./ChildThread/onValue_angle.js");
+const onValue_angle_Thread = new Worker(
+  `${__dirname}/ChildThread/onValue_angle.js`
+);
 
 //* オートロックセンサー設定の読込/更新を行う子スレッドを起動
 const onValue_autolock_sensor_Thread = new Worker(
-  "./ChildThread/onValue_autolock_sensor.js"
+  `${__dirname}/ChildThread/onValue_autolock_sensor.js`
 );
 
 //* オートロックタイム設定の読込/更新を行う子スレッドを起動
 const onValue_autolock_time_Thread = new Worker(
-  "./ChildThread/onValue_autolock_time.js"
+  `${__dirname}/ChildThread/onValue_autolock_time.js`
 );
 
 //* RealtimeDatabaseのIs_Lockedを読込/更新する子スレッドを起動
 const onValue_isLocked_Thread = new Worker(
-  "./ChildThread/onValue_isLocked.js",
+  `${__dirname}/ChildThread/onValue_isLocked.js`,
   { workerData: sharedArrayBuffer }
 );
 
 //* readSwitchのisOpenedを読込/更新する子スレッドを起動
-const readSwitch_Thread = new Worker("./ChildThread/readSwitch.js", {
+const readSwitch_Thread = new Worker(`${__dirname}/ChildThread/readSwitch.js`, {
   workerData: sharedArrayBuffer,
 });
 
